ea4a732a26d91ba57f524d6bddd98c13757caa67
tawakolineda-tech <tawakolineda@gmail.com>
2025-12-15 21:33:26 +0430

    part1 Novel

---
+++ part1.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../src/output.css">
+</head>
+<body class="bg-black">
+    <div class="w-full flex  justify-between flex-row bg-gray-700 rounded-b-4xl">
+       <nav class="flex justify-between w-full h-20 px-6 ">
+       <div class="w-full bg-gray-700 h-[60%] gap-11 p-4 text-3xl text-white rounded-b-4xl flex ">
+      <a href="home.php"><h1>→</h1></a>
+     <h1>دلدادگی شیطان</h1>
+    </div>
+       </nav> 
+    </div>
+    <div class="flex flex-row justify-between items-center w-full ">
+        <i class="mt-8 ">❤️</i>
+      <a href="kholasa.php" class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11"><button >خلاصه</button></a>
+        <i class="mt-8">❤️</i>
+        <button class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11 ">فصل اول</button> 
+</div>
+<div class="flex flex-row justify-between items-center w-full ">
+        <i class="mt-8 ">❤️</i>
+      <a href="kholasa.php" class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11"><button >فصل دوم</button></a>
+        <i class="mt-8">❤️</i>
+        <button class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11 ">فصل سوم</button> 
+</div>
+<div class="flex flex-row justify-between items-center w-full ">
+        <i class="mt-8 ">❤️</i>
+      <a href="kholasa.php" class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11"><button >فصل چهارم</button></a>
+        <i class="mt-8">❤️</i>
+        <button class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11 ">فصل پنجم</button> 
+</div>
+<div class="flex flex-row justify-between items-center w-full ">
+        <i class="mt-8 ">❤️</i>
+      <a href="kholasa.php" class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11"><button >فصل ششیم</button></a>
+        <i class="mt-8">❤️</i>
+        <button class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11 ">فصل هفتم</button> 
+</div>
+<div class="flex flex-row justify-between items-center w-full ">
+        <i class="mt-8 ">❤️</i>
+      <a href="kholasa.php" class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11"><button >فصل هشتم</button></a>
+        <i class="mt-8">❤️</i>
+        <button class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11 ">فصل نهم</button> 
+</div>
+<div class="flex flex-row justify-between items-center w-full ">
+        <i class="mt-8 ">❤️</i>
+      <a href="kholasa.php" class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11"><button >فصل دهم</button></a>
+        <i class="mt-8">❤️</i>
+        <button class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11 ">فصل یازدهم</button> 
+</div>
+<div class="flex flex-row justify-between items-center w-full ">
+        <i class="mt-8 ">❤️</i>
+      <a href="kholasa.php" class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11"><button >فصل دوازهم</button></a>
+        <i class="mt-8">❤️</i>
+        <button class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11 ">فصل سیزدهم</button> 
+</div>
+<div class="flex flex-row justify-between items-center w-full ">
+        <i class="mt-8 ">❤️</i>
+      <a href="kholasa.php" class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11"><button >فصل چهاردهم</button></a>
+        <i class="mt-8">❤️</i>
+        <button class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11 ">فصل پانزدهم</button> 
+</div>
+<div class="flex flex-row justify-between items-center w-full ">
+        <i class="mt-8 ">❤️</i>
+      <a href="kholasa.php" class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11"><button >فصل شند زدههم</button></a>
+        <i class="mt-8">❤️</i>
+        <button class="bg-gray-700 w-1/2 flex mt-8 flex-row justify-between rounded-4xl p-4 h-11 ">فصل هفدههم</button> 
+</div>
+</body>
+</html>
